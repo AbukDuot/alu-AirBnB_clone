430a8ebdfdc258bcb2b93db5f0dd5f6f80bfe2a7
Samuel Dushime <127239702+Samenergy@users.noreply.github.com>
2024-01-10 19:10:23 +0200

    Updated

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,16 +5,15 @@ import unittest
 from os import path
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-""" testing the file storage"""
-
 
 class TestCaseFileStorage(unittest.TestCase):
-    """ class for test cases """
-   def test_pep8_Filestorage(self):
-       """tests pep8"""
-       style = pep8.StyleGuide(quiet=True)
-       p  = style.check_files(['models/engine/file_storage.py'])
-       self.assertEqual(p.total_errors, 0, "check pep8")
+    """Class for test cases"""
+
+    def test_pep8_Filestorage(self):
+        """Tests pep8"""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0, "Check pep8")
        
     def setUp(self):
         """ setting up the various
